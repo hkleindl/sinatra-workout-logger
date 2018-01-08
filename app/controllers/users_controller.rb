e77398d0cecae44005b33546b80f179bb6357d05
Hunter Kleindl <hkleindl@gmail.com>
2018-01-08 17:30:19 +0000

    refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    if @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect '/workouts'
     else
