98012fc4e8ab78f7256c6e127e511952b258780f
Hunter Kleindl <hkleindl@gmail.com>
2018-01-08 19:29:21 +0000

    added conditions to post signup, added flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,28 @@
+require 'rack-flash'
+
 class UsersController < ApplicationController
+
+  use Rack::Flash
 
   get '/signup' do
     erb :'users/signup'
   end
 
   post '/signup' do
-    binding.pry
     if !params[:user].values.any?(&:empty?)
-      @user = User.create(params[:user])
-      session[:user_id] = @user.id
-      redirect '/workouts'
+      if !username_taken? && !email_taken?
+        @user = User.create(params[:user])
+        session[:user_id] = @user.id
+        redirect '/workouts'
+      elsif username_taken?
+        flash[:message] = "That username is already taken. Please choose a new one"
+        redirect '/signup'
+      elsif email_taken?
+        flash[:message] = "That email address is already being used."
+        redirect '/signup'
+      end
     else
+      flash[:message] = "Please fill in all fields."
       redirect '/signup'
     end
   end
