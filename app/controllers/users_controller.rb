49e16a022a8b5b9e5ae085df54a602cf7fd0f30d
Hunter Kleindl <hkleindl@gmail.com>
2018-01-08 16:14:31 +0000

    signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,13 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-
+    if !params[:user].values.any?(&:empty?)
+      @user = User.create(params[:user])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.slug}"
+    else
+      redirect '/signup'
+    end
   end
+
 end
