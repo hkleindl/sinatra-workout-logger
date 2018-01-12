987af606e069011d2344d4ff4f734e8c1be33fb8
Hunter Kleindl <hkleindl@gmail.com>
2018-01-12 16:00:50 +0000

    post exercises/cardio/new, get /exercises/cardio/add

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -15,4 +15,21 @@ class ExercisesController < ApplicationController
       redirect_if_not_logged_in
     end
   end
+
+  post '/exercises/cardio/new' do
+    if !params[:cardio][:name].empty?
+      @exercise = Exercise.create(params)
+      redirect '/exercises/cardio/add'
+    else
+      flash[:message] = "Please fill out name field"
+      redirect '/exercises/cardio/new'
+    end
+  end
+
+  get '/exercises/cardio/add' do
+    if logged_in?
+      erb :'/exercises/cardio/add'
+    else
+      redirect_if_not_logged_in
+  end
 end
