f93879ba02da15bbaac3fbf30897d46b80191a0d
Hunter Kleindl <hkleindl@gmail.com>
2018-01-13 18:03:48 +0000

    refactor

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -106,6 +106,7 @@ class ExercisesController < ApplicationController
 
   get '/exercises/cardio/:id/edit' do
     if logged_in?
+      @exercise = current_workout.exercises.find_by(id: params[:id])
       erb :'/exercises/cardio/edit'
     else
       redirect_if_not_logged_in
