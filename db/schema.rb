0a52680bf0d4b6d696ec8b78681106a5c5a351bb
Hunter Kleindl <hkleindl@gmail.com>
2018-01-15 16:49:50 +0000

    exercises_workouts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115155907) do
+ActiveRecord::Schema.define(version: 20180115164236) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "category"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20180115155907) do
     t.integer "weight"
     t.text "description"
     t.integer "user_id"
+  end
+
+  create_table "exerises_workouts", force: :cascade do |t|
+    t.integer "exercise_id"
+    t.integer "workout_id"
   end
 
   create_table "users", force: :cascade do |t|
